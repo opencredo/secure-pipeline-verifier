9ee40ff00a4e5a8ade32a27aeb70a71d1b848258
Hieu Doan <hiieu@users.noreply.github.com>
2021-10-13 15:35:52 +0100

    provide default timestamp for lastrun

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,28 +17,34 @@ resource "aws_s3_bucket" "secure_pipeline" {
 }
 
 resource "aws_s3_bucket_object" "config_file" {
-  bucket = aws_s3_bucket.secure-pipeline.bucket
+  bucket = aws_s3_bucket.secure_pipeline.bucket
   key    = "${var.repository}/config.yaml"
   source = var.config_file
 }
 
 resource "aws_s3_bucket_object" "trusted_data_file" {
-  bucket = aws_s3_bucket.secure-pipeline.bucket
+  bucket = aws_s3_bucket.secure_pipeline.bucket
   key    = "${var.repository}/trusted_data.json"
   source = var.trusted_data_file
 }
 
 resource "aws_s3_bucket_object" "policies" {
-  bucket   = aws_s3_bucket.secure-pipeline.bucket
+  bucket   = aws_s3_bucket.secure_pipeline.bucket
   for_each = fileset(var.policies_dir, "*.rego")
   key      = "${var.repository}/policies/${each.value}"
   source   = "${var.policies_dir}/${each.value}"
 }
 
 resource "aws_ssm_parameter" "last_run" {
-  description = "Last run of Secure Pipeline. Format: 'YYYY-MM-ddTHH:mm:ss.SSSZ'."
-  name  = "lambda/secure_pipeline/last_run"
+  description = "Last run of Secure Pipeline. Format: 'YYYY-MM-DD'T'hh:mm:ssZ'."
+  name  = "/Lambda/SecurePipelines/last_run"
   type  = "String"
-  value = "" // a single whitespace
-  overwrite = false
+  # If the value doesn't exist then the last run will be the deployment time of this resource.
+  value = timestamp()
+  lifecycle {
+    # Fill the value when the resource is created for the first time. Later it might be changed outside of Terraform.
+    ignore_changes = [
+      value,
+  ]
+  }
 }
