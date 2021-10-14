34d798496ee870800326a7f49262ec60498abb36
Hieu Doan <hiieu@users.noreply.github.com>
2021-10-14 08:53:43 +0100

    format main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,34 +17,40 @@ resource "aws_s3_bucket" "secure_pipeline" {
 }
 
 resource "aws_s3_bucket_object" "config_file" {
-  bucket = aws_s3_bucket.secure_pipeline.bucket
-  key    = "${var.repository}/config.yaml"
-  source = var.config_file
+  bucket      = aws_s3_bucket.secure_pipeline.bucket
+  key         = "${var.repository}/config.yaml"
+  source      = var.config_file
+  source_hash = filemd5(var.config_file)
+  depends_on  = [aws_s3_bucket.secure_pipeline]
 }
 
 resource "aws_s3_bucket_object" "trusted_data_file" {
-  bucket = aws_s3_bucket.secure_pipeline.bucket
-  key    = "${var.repository}/trusted_data.json"
-  source = var.trusted_data_file
+  bucket      = aws_s3_bucket.secure_pipeline.bucket
+  key         = "${var.repository}/trusted_data.json"
+  source      = var.trusted_data_file
+  source_hash = filemd5(var.trusted_data_file)
+  depends_on  = [aws_s3_bucket.secure_pipeline]
 }
 
 resource "aws_s3_bucket_object" "policies" {
-  bucket   = aws_s3_bucket.secure_pipeline.bucket
-  for_each = fileset(var.policies_dir, "*.rego")
-  key      = "${var.repository}/policies/${each.value}"
-  source   = "${var.policies_dir}/${each.value}"
+  bucket      = aws_s3_bucket.secure_pipeline.bucket
+  for_each    = fileset(var.policies_dir, "*.rego")
+  key         = "${var.repository}/policies/${each.value}"
+  source      = "${var.policies_dir}/${each.value}"
+  source_hash = filemd5("${var.policies_dir}/${each.value}")
+  depends_on  = [aws_s3_bucket.secure_pipeline]
 }
 
 resource "aws_ssm_parameter" "last_run" {
   description = "Last run of Secure Pipeline. Format: 'YYYY-MM-DD'T'hh:mm:ssZ'."
-  name  = "/Lambda/SecurePipelines/last_run"
-  type  = "String"
+  name        = "/Lambda/SecurePipelines/last_run"
+  type        = "String"
   # If the value doesn't exist then the last run will be the deployment time of this resource.
   value = timestamp()
   lifecycle {
     # Fill the value when the resource is created for the first time. Later it might be changed outside of Terraform.
     ignore_changes = [
       value,
-  ]
+    ]
   }
 }
