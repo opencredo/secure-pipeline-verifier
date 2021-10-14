b57a7f34e7ca5e2684ad71a3caff2167fce2a816
Hieu Doan <hiieu@users.noreply.github.com>
2021-10-14 16:00:16 +0100

    remove remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    region = "eu-west-2"
-    key    = "state_yo/terraform.tfstate"
-    bucket = "hieu-secure-pipeline-tf-state"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
