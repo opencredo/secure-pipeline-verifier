d4d265768ea3eb084ae3542dec1556881a643f94
Hieu Doan <hiieu@users.noreply.github.com>
2021-12-21 14:55:09 +0000

    chatops invokes lambda

--- cmd/aws/chatops/main.go
+++ cmd/aws/chatops/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"net/url"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	lsvc "github.com/aws/aws-sdk-go/service/lambda"
 )
 
 type slackRequest struct {
@@ -23,10 +29,20 @@ type slackRequest struct {
 	triggerID   string
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// Commands coming from Slack comes in one string
+type slackParameters struct {
+	Repo string
+	// TODO: We should be able to pass LastRun via slack
+	LastRun string
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// valification token
-	token := os.Getenv("VERIFICATION_TOKEN")
 	vals, _ := url.ParseQuery(request.Body)
 	req := slackRequest{
 		vals.Get("token"),
@@ -42,22 +58,39 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		vals.Get("trigger_id"),
 	}
 
-	if req.token != token {
-		// invalid token
-		return events.APIGatewayProxyResponse{
-			Body:       fmt.Sprintf("Invalid token."),
-			StatusCode: 401,
-		}, nil
+	args := strings.Fields(req.command)
+	params := slackParameters{
+		Repo: args[0],
 	}
 
-	// ### Write your command logic ###
+	newSession := session.Must(session.NewSession())
+	svc := lsvc.New(newSession)
+
+	body, err := json.Marshal(map[string]interface{}{
+		"region":     "eu-west-2",
+		"bucket":     "secure-pipeline-bucket",
+		"configPath": params.Repo,
+	})
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	input := &lsvc.InvokeInput{
+		FunctionName: aws.String("secure-pipeline"),
+		Payload:      body,
+	}
+	invoke, err := svc.Invoke(input)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("%v", req),
+		Body:       fmt.Sprintf("%v", invoke.Payload),
 		StatusCode: 200,
 	}, nil
 }
 
-func main() {
-	lambda.Start(handler)
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
 }
